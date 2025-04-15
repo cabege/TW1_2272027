a82a69a8f9d7529a2aecf3a2f6045f2f636cc8c9
Shim HeeYoon <entrance1002@gmail.com>
2025-04-15 11:58:06 +0900

    up

---
+++ Assets/2272027_SHY/TW1_2272027/D06_Item_Controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D06_Item_Controller : MonoBehaviour
+{
+
+    private void OnMouseDown()
+    {
+        PrintInfo(); 
+    }
+
+    void PrintInfo()
+    {
+        print($"{gameObject.name}을/를 클릭했습니다.");
+    }
+}
